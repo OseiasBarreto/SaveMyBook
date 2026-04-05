d2b444ef81a6f3871132ced575ae072aef7b4088
Oséias Barreto <Oseiasbarreto94@gmail.com>
2026-04-04 23:44:05 -0300

    first commit

---
+++ src/main/java/com/barreto/oseias/savemybook/SavemybookApplication.java
@@ -0,0 +1,13 @@
+package com.barreto.oseias.savemybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavemybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavemybookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barreto/oseias/savemybook/SavemybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barreto.oseias.savemybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavemybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
